984a80bbeb87e61435611d6a79aa4e015b8c136c
Tyler Sampson <tyler_sampson@tresblue.com>
2015-05-23 10:52:13 +0200

    Log exception in decode

--- lib/mdi_cloud_decoder/core.rb
+++ lib/mdi_cloud_decoder/core.rb
@@ -2,7 +2,8 @@ module MdiCloudDecoder
   def self.decode(input)
     raw_data = JSON.parse(input)        
     MData.new(raw_data)
-  rescue
+  rescue Exception => ex
+    puts ex.to_s
     nil
   end 
 end
