5ed7e7430173201d6df9fb30b9056d37984c661b
Tyler Sampson <tyler_sampson@tresblue.com>
2015-05-23 11:12:09 +0200

    Return null on decode failure

--- lib/mdi_cloud_decoder/core.rb
+++ lib/mdi_cloud_decoder/core.rb
@@ -2,8 +2,7 @@ module MdiCloudDecoder
   def self.decode(input)
     raw_data = JSON.parse(input)        
     MData.new(raw_data)
-  rescue Exception => ex
-    puts ex.to_s
+  rescue
     nil
   end 
 end
