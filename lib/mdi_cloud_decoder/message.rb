b9978484bce8a9a0e7ebfe02d6d327e98f53b30e
Tyler Sampson <tyler_sampson@tresblue.com>
2015-05-23 11:09:21 +0200

    Remove created_at field for messages

--- lib/mdi_cloud_decoder/message.rb
+++ lib/mdi_cloud_decoder/message.rb
@@ -3,7 +3,6 @@ module MdiCloudDecoder
     attr_reader :id, 
       :asset, 
       :channel, 
-      :created_at, 
       :received_at, 
       :recorded_at, 
       :parent_id, 
@@ -19,7 +18,6 @@ module MdiCloudDecoder
             
       @asset = json['asset']
       @channel = json['channel']
-      @created_at = Time.parse(json['created_at'])
       @parent_id = json['parent_id']
       @b64_payload = json['b64_payload']
       @payload_str = Base64.decode64(@b64_payload)
