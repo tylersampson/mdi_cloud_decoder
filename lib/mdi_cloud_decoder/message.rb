702882fb3294956c8b53d28d602af75048cefbaa
Tyler Sampson <tyler_sampson@tresblue.com>
2015-05-23 10:18:19 +0200

    Parse presences and messages

--- lib/mdi_cloud_decoder/message.rb
+++ lib/mdi_cloud_decoder/message.rb
@@ -1,4 +1,37 @@
 module MdiCloudDecoder  
   class Message
+    attr_reader :id, 
+      :asset, 
+      :channel, 
+      :created_at, 
+      :received_at, 
+      :recorded_at, 
+      :parent_id, 
+      :b64_payload, 
+      :payload_str, 
+      :sender, 
+      :recipient, 
+      :type, 
+      :thread_id
+    
+    def initialize(json)      
+      @id = json['id']
+            
+      @asset = json['asset']
+      @channel = json['channel']
+      @created_at = Time.parse(json['created_at'])
+      @parent_id = json['parent_id']
+      @b64_payload = json['b64_payload']
+      @payload_str = Base64.decode64(@b64_payload)
+      
+      @received_at = Time.parse(json['received_at'])
+      @recorded_at = Time.parse(json['recorded_at'])
+            
+      @sender = json['sender']
+      @recipient = json['recipient']
+      @type = json['type']
+      
+      @thread_id = json['thread_id']
+    end
   end  
 end
--- lib/mdi_cloud_decoder/presence.rb
+++ lib/mdi_cloud_decoder/presence.rb
@@ -1,4 +1,16 @@
 module MdiCloudDecoder  
   class Presence
-  end
+    attr_reader :id, 
+      :asset,
+      :time,
+      :type,
+      :reason
+    
+    def initialize(json)
+      @id = json['id']      
+      @asset = json['asset']
+      @time = Time.parse(json['time'])
+      @reason = json['reason']
+    end
+  end  
 end
